809d47f9ac6bbeb5cd29221b984601617a93710c
TheWoollySheep <thewoollysheep@icloud.com>
2017-01-31 19:10:38 -0800

    refactored windowgui

--- windowgui.py
+++ windowgui.py
@@ -2,11 +2,8 @@ from tkinter import *
 
 root = Tk()
 
-def inputName():
-    inputName = str("Name: ")
-    return name
 
-label_1 = Label(root, inputName)
+label_1 = Label(root,  "Name: ")
 label_1.grid(row=0, sticky=E)
 label_1.pack()
 
